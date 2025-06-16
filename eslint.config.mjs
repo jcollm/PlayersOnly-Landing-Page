74d6c6e90e9acad93c95415e23d1f3bf6a742ec5
Josh Collins <collinsj8244@gmail.com>
2025-06-16 03:51:12 -0500

    chore: eslint config changed to treat unused variables as a warning rather than an error, for the purposes of import statements

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // custom rules
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn", // Change from "error" to "warn"
+    },
+  },
 ];
 
 export default eslintConfig;
